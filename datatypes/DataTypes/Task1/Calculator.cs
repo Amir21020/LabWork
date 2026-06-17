cb600a840ca194b03ed291e8f61c1999d0f43f04
Amir Amirov <mergalievradmir484@gmail.com>
2026-06-17 09:10:58 +0300

    feat: add data types project

---
+++ datatypes/DataTypes/Task1/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace DataTypes.Task1;
+
+public static class Calculator
+{
+    public static string CalculateCompoundInterest(decimal initialDeposit, int years, decimal interestRate)
+    {
+        StringBuilder result = new();
+        decimal balance = initialDeposit;
+        for (int year = 1; year <= years; year++)
+        {
+            balance += (balance * interestRate) / 100;
+
+            string formattedBalance = balance.ToString("F2", CultureInfo.InvariantCulture);
+            result.AppendLine($"Год {year}: {formattedBalance} руб.");
+        }
+        return result.ToString().TrimEnd();
+    }
+}
---
+++ datatypes/DataTypes/Task2/DiamondPrinter.cs
@@ -0,0 +1,33 @@
+﻿namespace DataTypes.Task2;
+
+public static class DiamondPrinter
+{
+    public static void GenerateDiamondFromSymbolX(int N)
+    {
+        var spaces = new string[N, N];
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                spaces[i, j] = " ";
+
+        var center = (N - 1) / 2;
+
+        for (int r = 0; r < N; r++)
+        {
+            int offset = center - Math.Abs(center - r);
+
+            spaces[r, center + offset] = spaces[r, center - offset] = "x";
+        }
+
+        spaces[center, center] = " ";
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(spaces[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
